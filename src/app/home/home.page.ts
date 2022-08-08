f267efb4bd5fc4a0b0be34a7ef14237699bc8b6b
Tomislav Markovica <tomislav.markovica1@gmail.com>
2022-08-08 20:46:54 +0200

    Animations and hyperlinks added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Typewriter from 't-writer.js'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  private headings: string[] = ["Biography", "Skills", "Language Skills", "My Web Projects"];
 
   constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    const target = document.querySelector('.typewriter');
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeColor: 'blue'
+    });
+    writer
+      .type('Portfolio')
+      .rest(2000)
+      .start();
+
+    var targetHeadings: NodeListOf<Element> = document.querySelectorAll('.typewriterHeadings')
+    let index = 0;
+    targetHeadings.forEach(element => {
+      const writer1 = new Typewriter(element, {
+        loop: false,
+        typeColor: 'white'
+      });
+      writer1.type(this.headings[index]).start()
+      index++;
+    });
+  }
 
   goToContactPage() {
     this.router.navigate(["contact"], {replaceUrl: false});
